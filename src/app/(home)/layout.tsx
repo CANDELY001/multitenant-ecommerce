3c0e3ec6b12c1d39123da0cfb5d15cd8f1f341fe
CANDELY001 <candely1charifa@gmail.com>
2025-09-05 22:05:49 +0100

    Added home Layout

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,17 @@
+interface layoutProps {
+  children: React.ReactNode;
+}
+import { Footer } from "./footer";
+import { Navbar } from "./navbar";
+
+const layout = ({ children }: layoutProps) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <div className="flex-1 bg-[#F4F4F4]">{children}</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default layout;
