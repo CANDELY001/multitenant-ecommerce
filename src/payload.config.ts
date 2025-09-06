8090ccbc39b1a4f43fcb9c7f544c6a1e880d924e
CANDELY001 <candely1charifa@gmail.com>
2025-09-06 01:08:49 +0100

    payload integration

---
+++ src/payload.config.ts
@@ -0,0 +1,37 @@
+// storage-adapter-import-placeholder
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import path from "path";
+import { buildConfig } from "payload";
+import { fileURLToPath } from "url";
+
+import { Users } from "./collections/Users";
+import { Media } from "./collections/Media";
+import { Categories } from "./collections/Categories";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    importMap: {
+      baseDir: path.resolve(dirname),
+    },
+  },
+  collections: [Users, Media, Categories],
+  editor: lexicalEditor(),
+  secret: process.env.PAYLOAD_SECRET || "",
+  typescript: {
+    declare: false,
+    outputFile: path.resolve(dirname, "payload-types.ts"),
+  },
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI || "",
+  }),
+  plugins: [
+    payloadCloudPlugin(),
+    // storage-adapter-placeholder
+  ],
+});
