0a245f9c1c044e340ca9ffdfbc6bcdd45805cfa9
CANDELY001 <candely1charifa@gmail.com>
2025-09-07 23:08:50 +0100

    Added filter products fix

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -17,10 +17,13 @@ export const productRouter = createTRPCRouter({
       const where: Where = {};
 
       if (input.minPrice) {
-        where.price = { greater_than_equal: input.minPrice };
+        where.price = { ...where.price, greater_than_equal: input.minPrice };
       }
       if (input.maxPrice) {
-        where.price = { less_than_equal: input.maxPrice };
+        where.price = {
+          ...where.price,
+          less_than_equal: input.maxPrice,
+        };
       }
       if (input.category) {
         const categoriesData = await ctx.payload.find({
